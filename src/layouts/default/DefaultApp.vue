0fe8b2b159396a064b860708dd6bbe14ff9e54a5
Viniciuscpires <2viniciuscpires@gmail.com>
2023-02-18 20:31:50 -0300

    Added fetch for exaple

--- src/layouts/default/DefaultApp.vue
+++ src/layouts/default/DefaultApp.vue
@@ -10,4 +10,8 @@
 import AppBar from './AppBar.vue'
 import SideBar from './SideBar.vue'
 import DefaultView from './DefaultView.vue'
+
+fetch('https://viniciuscpires.github.io/cartinhasCblol/info.json')
+  .then((response) => response.json())
+  .then((data) => console.log(data))
 </script>
