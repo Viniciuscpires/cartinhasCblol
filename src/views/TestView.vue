c42721077c2ecca2001d853c60145677ac2b4d5f
Viniciuscpires <2viniciuscpires@gmail.com>
2023-02-19 12:09:52 -0300

    Fix test view

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -6,11 +6,13 @@ import CartinhaItem from '../components/CartinhaItem.vue'
   <main class="tw-p-16">
     <CartinhaItem
       value="80"
-      countryId="1"
-      team="loud"
-      name="Gabriel Nemeth"
-      nick="Makes"
-      role="Top"
+      countryFlag="🇧🇷"
+      summonerName="Hidan"
+      fullName="Leonardo Santos"
+      role="top"
+      image="http://static.lolesports.com/players/1674503162464_Hidan-1copy.png"
+      teamIcon="http://static.lolesports.com/teams/1671220578076_LOSGRANDES1.png"
+      teamBackground="http://static.lolesports.com/teams/1671220578079_LOSGRANDES1.png"
     />
 
     <div class="tw-w-64 tw-mt-16">
