29e6c134d043d9e56cdf710096a384911a78003d
Viniciuscpires <2viniciuscpires@gmail.com>
2023-02-19 11:17:35 -0300

    Remove test

--- src/layouts/default/DefaultApp.vue
+++ src/layouts/default/DefaultApp.vue
@@ -10,8 +10,4 @@
 import AppBar from './AppBar.vue'
 import SideBar from './SideBar.vue'
 import DefaultView from './DefaultView.vue'
-
-fetch('http://local.projetin.com:5173/cartinhasCblol/src/assets/info.json')
-  .then((response) => response.json())
-  .then((data) => console.log(data))
 </script>
